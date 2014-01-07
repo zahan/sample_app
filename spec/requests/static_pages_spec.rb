bbf15458cd4add658b454eb4f1d43b3010845fcd
Paul Stets <this.is.paul.st@gmail.com>
2014-01-08 01:10:14 +0600

    Add iteractive title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,11 @@ describe "Static pages" do
       page.should have_selector('title',
                         :text => "#{base_titile} | Home")
     end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
 
   describe "Help page" do
