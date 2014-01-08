8cd22c8cb14c03e37650e1239f8ab97efca81cae
Paul Stets <this.is.paul.st@gmail.com>
2014-01-09 03:52:23 +0600

    Add routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,68 +2,60 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_titile) { "Ruby on Rails Tutorial Sample App" }
-
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      visit root_path
+      expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "#{base_titile} | Home")
+    it "should have the base title" do
+      visit root_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
-    
+
     it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
+      visit root_path
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      visit help_path
+      expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "#{base_titile} | Help")
+      visit help_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                    :text => "#{base_titile} | About Us")
+      visit about_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
-  describe "Contacts page" do
+  describe "Contact page" do
 
-  	it "dolzhen bit h1 'Contact Us'" do
-  		visit '/static_pages/contacts'
-  		page.should have_selector('h1', :text => 'Contact Us')
-	end
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
 
-	it "should have the title 'Contact Us'" do
-		visit '/static_pages/contacts'
-		page.should have_selector('title',
-						:text => "#{base_titile} | Contact Us")
-	end
-
-   end
-
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
